536dedaa61cd1350403ceb2383c0896f1e1ded53
raggzorpages <ragnar_boe@hotmail.com>
2024-10-09 19:25:10 +0200

    dsa

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-<script>
-  // Initialize Supabase
-  const { createClient } = supabase;
 const supabaseUrl = 'https://gbxerteotnzpnbodndao.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdieGVydGVvdG56cG5ib2RuZGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0ODMwMDUsImV4cCI6MjA0NDA1OTAwNX0.k2FdBvntYLUOCw7kSZvEDeH0iyNFlQXLxkQk9Ln34bg';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);  // Initialize Supabase client
-
-</script>
